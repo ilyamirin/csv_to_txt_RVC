565f346ab6208b4c00420db401d5bb2a224c6c78
Ilya G. Mirin <ilya.mirin@gmail.com>
2020-06-28 15:45:52 +1000

    adds metha-generation

--- CsvToText.py
+++ CsvToText.py
@@ -4,15 +4,20 @@ import os
 import datetime
 
 
-def create_file(file_name, file_body):
+def create_meta(file_dict):
+    return "Тема: (* %(theme)s *)\nКласс: 1 курс\nГод: 2020\nПредмет: английский\nТест: егэ тренировка\nЭксперт: " \
+           "%(Exp)s\n" % file_dict
+
+
+def create_file(file_name, file_meta, file_body):
     print(file_name)
     f = open(file_name, "w+", encoding='utf-8')
-    for i in file_body:
-        f.write(file_body.trim() + "\r\n")
+    f.write(file_meta)
+    f.write(file_body)
     f.close()
 
 
-data = pd.read_csv("500.csv", delimiter=';')
+data = pd.read_csv("551-1050.csv", delimiter=';')
 
 prefix = u'%0d%1s' % (datetime.datetime.utcnow().timestamp(), os.path.sep)
 os.mkdir(prefix)
@@ -24,12 +29,13 @@ for row in data.iterrows():
         print(file_dict)
         file_dict['theme'] = u'_'.join(file_dict['body'].split()[:5])
 
-    file_dict['theme'] = re.sub(r'(,|\.|!|\?|;|\s|\\|/|"|%|:|<|>|\*)', u'_', file_dict['theme'], 100)
-    file_dict['theme'] = re.sub(r'_$', u'', file_dict['theme'], 1)
-    file_dict['theme'] = re.sub(r'_{2}', u'_', file_dict['theme'], 1)
-    #print(file_dict['theme'])
+    file_dict['file_theme'] = re.sub(r'(,|\.|!|\?|;|\s|\\|/|"|%|:|<|>|\*)', u'_', file_dict['theme'], 100)
+    file_dict['file_theme'] = re.sub(r'_$', u'', file_dict['file_theme'], 1)
+    file_dict['file_theme'] = re.sub(r'_{2}', u'_', file_dict['file_theme'], 1)
+    #print(file_dict)
 
-    filename = u'%(number)d_en_%(theme)s_%(Exp)s.txt' % file_dict
-    #print(filename)
-    create_file(prefix + filename, file_dict['body'])
+    filename = u'%(number)d_en_%(file_theme)s_%(Exp)s.txt' % file_dict
+    file_dict['meta'] = create_meta(file_dict)
+
+    create_file(prefix + filename, file_dict['meta'], file_dict['body'])
 
