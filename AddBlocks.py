8318245f5e3e1a39bb142da5d24b4dd299937a07
Ilya G. Mirin <ilya.mirin@gmail.com>
2020-09-08 16:10:01 +1000

    adds automatic problem marker

--- AddBlocks.py
+++ AddBlocks.py
@@ -5,14 +5,31 @@ import datetime
 import codecs
 from nltk import tokenize
 
-OPINIONS = [r'(I|My|my)[^,]+opinion[^s]', r'I[^,]+(think|believe|agree)[^s]', r'(I|i)n my mind',
-            r'(I|i)t would seem that', r'(T|t)h(is|at) (proves|suggests|supports) th(at|e)', r'(I|i)t could be argued']
+PERSONAL_OPINIONS = [r'(I|My|my)[^,]+opinion[^s]', r'I[^,]+(think|believe|agree)[^s]', r'(I|i)n my mind',
+                     r'(I|i)t would seem that', r'(T|t)h(is|at) (proves|suggests|supports) th(at|e)',
+                     r'(I|i)t could be argued']
 
 
-def get_opinion_matches(s: str) -> list:
+def get_personal_opinion_matches(s: str) -> list:
     res = list()
-    for op in OPINIONS:
+    for op in PERSONAL_OPINIONS:
         res.append(re.search(op, s))
+    return res
+
+
+def get_problem_matches(p_block: str) -> list:
+    res = list()
+    sentences = tokenize.sent_tokenize(p_block)
+    if len(sentences) < 2:
+        return res
+    m1 = re.search(r'\?$', sentences[-1])
+    m2 = re.search(r'\?$', sentences[-2:-1][0])
+    if not (m1 or m2):
+        m = re.search(r'(his essay|estion is)', p_block)
+        if m:
+            res.append(sentences[-2:-1])
+    else:
+        res.append(sentences[-2:-1])
     return res
 
 
@@ -22,32 +39,33 @@ raw_data = dict()
 for root, dirs, files in os.walk(path_to_dir):
     for filename in files:
         f = codecs.open(path_to_dir + os.sep + filename, "r", "utf-8")
-        raw_data[filename] = list(map(lambda s: s.strip(),f.read().split('\n')[7:]))
+        raw_data[filename] = list(map(lambda s: s.strip(), f.read().split('\n')[7:]))
 
 print(raw_data['0050477_en_Nowadays_the_problem_of_the_spread_of_coronovirus_infection_noexp.txt'])
 
 # analyse
 for text in raw_data.values():
-    for block in text:
-        for sentence in block.split('.'):
-            if sentence.count('his proves that') > 0:
-                print(sentence.strip())
-
+    for block in text[1:-1]:
+        print(block)
+        # for sentence in block.split('.'):
+        #    if sentence.count('his proves that') > 0:
+        #        print(sentence.strip())
 
 # working
 for text in raw_data.values():
     for block in text:
         for sentence in tokenize.sent_tokenize(block):
-            for m in get_opinion_matches(sentence):
+            for m in get_personal_opinion_matches(sentence):
                 if m:
                     print(sentence)
 
-# final
+# FINAL
 for text in raw_data.values():
     for block in text:
         for sentence in tokenize.sent_tokenize(block):
-            for m in get_opinion_matches(sentence):
+            for m in get_personal_opinion_matches(sentence):
                 if m:
                     print(sentence)
 
-
+    block = text[0]
+    print(get_problem_matches(block))
