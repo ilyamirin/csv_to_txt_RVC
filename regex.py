c9fe680210f9767cdde316fa608527c49e516b0e
Ilya G. Mirin <ilya.mirin@gmail.com>
2020-09-15 12:19:47 +1000

    fixes () problem in regex file

--- regex.py
+++ regex.py
@@ -1,16 +1,19 @@
 import re
 import codecs
 
-f = codecs.open('50101_en_The_major_disease_exp101.txt', "r", "utf-8")
+f = codecs.open('C:\\Users\\Admin\\Projects\\CsvToText\\added_texts\\0050009_en_An_early_choice_of_a_carrier_path_is_the_key_to_success_Exp105.txt', "r", "utf-8")
 text = f.read()
+
+text = re.sub(r'(?<=[^\*\\])\)', ']', text)
+text = re.sub(r'\((?=[^\*\\])', '[', text)
 
 print('-------------')
 
 i = 0
-for j in range(0,5):
-    for m in re.finditer(r'\(\\[^\(\)]+\)', text):
+for j in range(0, 12):
+    for m in re.finditer(r'\(\\[^\(\)]+\\\)', text):
         g = m.group()
-        text = re.sub(re.escape(g), '{%d}'%i, text) 
+        text = re.sub(re.escape(g), '{%d}' % i, text)
         print(i, g, m.start(), m.end())
         i += 1
     i *= 10
